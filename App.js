b4d389e336f0dd34bc653088495649927cea342b
c4hun <chun.4mtgu@simplelogin.fr>
2024-01-25 23:25:07 +0100

    init commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View Style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
